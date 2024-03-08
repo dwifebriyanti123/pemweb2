732f02db632504a74baba1df2e670101f7b21e1c
dwifebriyanti123 <dwif36645@gmail.com>
2024-03-08 10:56:06 +0700

    praktikum3

---
+++ praktikum3/Proses_registrasi.php
@@ -0,0 +1,12 @@
+<?php
+// Array numurik - index & volue
+$domisili = ["jakarta","Bogor","Bima","Depok","Bekasi","Tangerang"];
+
+// Array Assosialtive - key & volue
+$program_studi = [
+    "SI" => "Sistem informasi",
+    "TI" => "Teknik informatika",
+    "BD" => "Bisnin Dijital"
+];
+
+?>
